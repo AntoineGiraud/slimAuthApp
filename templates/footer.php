ca7776d9623ca0df9bc8190e2f1cc6e1d5dfb7dc
AntoineGiraud <antoine.giraud@outlook.com>
2016-06-01 12:15:20 -0400

    fix, les js sont dans le dossier public

--- templates/footer.php
+++ templates/footer.php
@@ -13,12 +13,12 @@
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="<?= $RouteHelper->publicPath ?>js/bootstrap.min.js"></script>
-    
+
     <?php if (!empty($js_for_layout)): ?>
       <?php foreach ($js_for_layout as $v):?>
-        <?php if (file_exists('js/'.$v.'.js')){ ?>
+        <?php if (file_exists($RouteHelper->publicPath.'js/'.$v.'.js')){ ?>
           <script src="<?= $RouteHelper->publicPath ?>js/<?= $v; ?>.js"></script>
-        <?php }elseif(file_exists('js/'.$v)){ ?>
+        <?php }elseif(file_exists($RouteHelper->publicPath.'js/'.$v)){ ?>
           <script src="<?= $RouteHelper->publicPath ?>js/<?= $v; ?>"></script>
         <?php }elseif(false !== strpos($v, '<script type="text/javascript">')){ ?>
             <?= $v ?>
