bc3c9ec470f85587fe024ce14f21e84825368939
AntoineGiraud <antoine.giraud@outlook.com>
2017-02-02 08:44:00 -0500

    fix middleware

--- src/middleware.php
+++ src/middleware.php
@@ -5,7 +5,10 @@
 
 // sécuriser l'application à l'aide d'une authentification
 $app->add(function ($request, $response, $next) {
-    if (!in_array($request->getUri()->getPath(), ['login'])) { // Toute autre page que login on check si la personne est bien authentifiée
+    $curPagePath = $request->getUri()->getPath();
+    if ($curPagePath != '/' && substr($curPagePath, 0, 1) == "/")
+        $curPagePath = substr($curPagePath, 1);
+    if (!in_array($curPagePath, ['login'])) { // Toute autre page que login on check si la personne est bien authentifiée
         if(!$this->Auth->isLogged()) {
             // Il n'était pas encore connecté en tant qu'icam.
             $this->flash->addMessage('info', "Vous devez être connecté pour accéder au reste de l'application");
@@ -14,11 +17,11 @@ $app->add(function ($request, $response, $next) {
     }
     // On a vérifié que l'utilisateur est connecté !
     // On va s'assurer qu'il a le droit d'accéder aux pages
-    if (in_array($request->getUri()->getPath(), $this->Auth->baseAllowedPages)) {
+    if (in_array($curPagePath, $this->Auth->baseAllowedPages)) {
         // On ne fait rien de spécial, mais ce sont les pages de base auquel tout user a le droit d'accèder
     } else if (!$this->Auth->isSuperAdmin()){
         // checker les droits
-        if (!$this->Auth->memberCanAccessPages($request->getUri()->getPath())) {
+        if (!$this->Auth->memberCanAccessPages($curPagePath)) {
             return $this->Auth->forbidden($response, $this->router, 'home');
         }
     }
